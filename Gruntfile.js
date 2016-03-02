e649d57d3aa3d5ccdf044fbd3ab6884f36252419
1na <louis.chaillet@gmail.com>
2016-03-02 21:31:32 +0100

    add new Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,20 @@
-grunt.initConfig({
-  respimg: {
-    default: {
-      options: {
-        widths: [200, 400]
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+      respimg: {
+        default: {
+          options: {
+            widths: [200, 400]
+          },
+          files: [{
+            expand: true,
+            cwd: 'image/',
+            src: ['**.{gif,jpg,png,svg}'],
+            dest: 'build/img/'
+          }]
+        }
       },
-      files: [{
-        expand: true,
-        cwd: 'src/img/',
-        src: ['**.{gif,jpg,png,svg}'],
-        dest: 'build/img/'
-      }]
-    }
-  },
-});
-grunt.loadNpmTasks('grunt-respimg');
+    });
+    grunt.loadNpmTasks('grunt-respimg');
+    grunt.registerTask('default', ['respimg']);
+};
