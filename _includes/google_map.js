1955dfffd88acfa187c2989f7fd127c809972301
1na <commerce.chaillet@gmail.com>
2016-09-22 20:42:34 +0200

    haha changed the color on the map

--- _includes/google_map.js
+++ _includes/google_map.js
@@ -3,7 +3,19 @@
   var mapDiv = document.getElementById('map');
   var map = new google.maps.Map(mapDiv, {
     center: myLatLng,
-    zoom: 16
+    zoom: 15,
+    styles:
+[
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#008751"
+      }
+    ]
+  }
+]
   });
   var marker = new google.maps.Marker({
     position: myLatLng,
